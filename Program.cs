5cf5fcb9158a4c9d9e4653cdf970250c88a65a50
Mat. Devski Carlier <carliermateo@outlook.be>
2023-10-03 19:20:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Generieke_Collecties;
+
+Stapel<int> integers = new();
+
+//voeg een methode toe aan de delegate van de event
+integers.OnWijziging += boodschapVermelding;
+
+integers.Opzetten(15);
+integers.Opzetten(63);
+integers.Afhalen();
+integers.Leegmaken();
+
+Console.ReadLine(); //sluit de applicatie niet meteen
+
+//een voorbeeldmethode om op de event te reageren
+void boodschapVermelding(object sender, string methode)
+{
+    Console.WriteLine("Een eigenschap van het object " + sender.ToString() + " werd gewijzigd met methode '" + methode + "'");
+}
---
+++ Stapel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generieke_Collecties
+{
+    //creeer een delegate voor de event
+    public delegate void WijzigingEventHandeler(object sender, string methode);
+
+    internal class Stapel<T>
+    {
+        List<T> container;
+        //declareer een event die gelinkt is aan de delegate
+        public event WijzigingEventHandeler OnWijziging;
+
+        public Stapel()
+        {
+            container = new List<T>();
+        }
+
+        public void Opzetten(T t) {
+            container.Add(t);
+
+            if (OnWijziging != null)
+            {
+                OnWijziging(this, "opzetten");
+            }
+        }
+
+        public T Afhalen() {
+            T item = container[^1];
+            container.RemoveAt(container.Count - 1);
+
+            if (OnWijziging != null)
+            {
+                OnWijziging(this, "afhalen");
+            }
+            return item;
+        }
+
+        public void Leegmaken() {
+            container = new List<T>();
+
+            if (OnWijziging != null)
+            {
+                OnWijziging(this, "leegmaken");
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "[" + string.Join(",", container) + "]";
+
+            return "Stapel{" + s + "}";
+        }
+
+    }
+}
